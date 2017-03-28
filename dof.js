19c04fd3026bd790660f55bd9088b49ea69196f1
jambun <jambun@gmail.com>
2017-03-29 00:14:59 +1100

    current settings in the cookie too, and a refactor

--- dof.js
+++ dof.js
@@ -1,6 +1,6 @@
 
 function DepthOfFieldCalculator() {
-    this.coc;
+    this.CIRCLE_OF_CONFUSION;
 
     this.FORMATS = {
 	"apsc": [15, 22.5],
@@ -16,8 +16,6 @@ function DepthOfFieldCalculator() {
 	"8x10": [203, 254]
     };
 
-    this.COOKIE = {"formats": {}};
-
     this.FOCAL_LENGTHS = [11, 15, 20, 21, 24, 28, 35, 40, 50, 55, 75, 85, 90, 100, 105,
 			  135, 150, 180, 200, 210, 240, 250, 270, 350, 400, 450, 600, 720,
 			  1000, 1200, 1700, 2000];
@@ -29,10 +27,17 @@ function DepthOfFieldCalculator() {
 				 3.2, 3.5, 4, 4.5, 5.0, 5.6, 6.3, 7.1, 8,
 				 9, 10, 11, 13, 14, 16, 18, 20, 22, 25, 29, 32, 36, 40, 45];
 
+    this.COOKIE = {"formats": {},
+		   "settings": {"format": "35mm",
+				"focal_length": this.FOCAL_LENGTHS[8],
+				"aperture": this.APERTURES[6],
+				"distance": "",
+				"denominator": "1730"}};
+
     this.FOCUS_RING = [
-		       document.querySelector("input[name=distance]"),
-		       document.querySelector("input[name=aperture]"),
-		       document.querySelector("input[name=focal_length]"),
+		       this.setting_elt("distance"),
+		       this.setting_elt("aperture"),
+		       this.setting_elt("focal_length"),
 		       ];
     this.FOCUS_INDEX = 0;
 
@@ -48,17 +53,18 @@ DepthOfFieldCalculator.prototype.init = function() {
     var self = this;
 
     self.read_cookie();
-
-    for (var k in self.COOKIE['formats']) {
-	let f = self.COOKIE['formats'][k];
+    for (var k in self.COOKIE["formats"]) {
+	let f = self.COOKIE["formats"][k];
 	self.add_format(k, f[0], f[1]);
     }
+
+    self.apply_settings(self.COOKIE["settings"]);
 
     var dofinput = document.querySelectorAll("input.dofinput");
     for (var i = 0; i < dofinput.length; i++) {
 	dofinput[i].setAttribute("readonly", true);
 	dofinput[i].oninput = function() {
-	    self.calculate();
+	    self.setting_changed(this.name);
 	}
 	dofinput[i].onclick = function() {
 	    this.removeAttribute("readonly");
@@ -82,14 +88,12 @@ DepthOfFieldCalculator.prototype.init = function() {
 	    document.getElementById("add-format").style.display = "inline";
 	    document.querySelector("input.addinput[name=name]").focus();
 	} else {
-	    self.circle_of_confusion();
-	    self.calculate();
+	    self.setting_changed("format");
 	}
     }
 
-    document.querySelector("select[name=denominator]").onchange = function() {
-	self.circle_of_confusion();
-	self.calculate();
+    self.setting_elt("denominator").onchange = function() {
+	self.setting_changed("denominator");
     }
 
     document.getElementById("add-format-btn").onclick = function() {
@@ -186,16 +190,39 @@ DepthOfFieldCalculator.prototype.init = function() {
 	self.set_next_stop(true, 2);
     }
 
-    this.setup_keyboard_handling();
+    self.setup_keyboard_handling();
 
-    document.querySelector("select[name=format]").value = "35mm";
-    this.circle_of_confusion();
-    document.querySelector("input[name=focal_length]").value = this.FOCAL_LENGTHS[8];
-    document.querySelector("input[name=aperture]").value = this.APERTURES[6];
+    self.circle_of_confusion();
+
     self.calculate();
 
-    document.querySelector("input[name=distance]").value = "";
-    document.querySelector("input[name=distance]").focus();
+    self.setting_elt("distance").focus();
+}
+
+
+DepthOfFieldCalculator.prototype.setting_elt = function(setting_name) {
+    return document.querySelector(".dofinput[name=" + setting_name + "]");
+}
+
+
+DepthOfFieldCalculator.prototype.setting_val = function(setting_name) {
+    return this.setting_elt(setting_name).value;
+}
+
+
+DepthOfFieldCalculator.prototype.change_setting = function(setting_name, value) {
+    this.setting_elt(setting_name).value = value;
+    this.setting_changed(setting_name);
+}
+
+
+DepthOfFieldCalculator.prototype.setting_changed = function(setting_name) {
+    this.COOKIE["settings"][setting_name] = this.setting_elt(setting_name).value;
+    this.write_cookie();
+    if (setting_name == "denominator" || setting_name == "format") {
+	this.circle_of_confusion();
+    }
+    this.calculate();
 }
 
 
@@ -226,19 +253,36 @@ DepthOfFieldCalculator.prototype.read_cookie = function() {
     let cookie = document.cookie;
     cookie = cookie.replace(/data=(.*);?$/, "$1");
     if (cookie) {
-	this.COOKIE = JSON.parse(atob(cookie));
+	let new_cookie = JSON.parse(atob(cookie));
+	for (k in this.COOKIE) {
+	    if (typeof new_cookie[k] == 'undefined') {
+		new_cookie[k] = this.COOKIE[k];
+	    }
+	}
+	this.COOKIE = new_cookie;
+    }
+}
+
+
+DepthOfFieldCalculator.prototype.apply_settings = function(settings) {
+    for (k in settings) {
+	if (this.setting_elt(k)) {
+	    this.change_setting(k, settings[k]);
+	}
     }
 }
 
 
 DepthOfFieldCalculator.prototype.circle_of_confusion = function() {
-    var fdims = this.FORMATS[document.querySelector("select[name=format]").value];
+    var fdims = this.FORMATS[this.setting_val("format")];
     var diagonal = Math.sqrt(Math.pow(fdims[0], 2) + Math.pow(fdims[1], 2));
-    var denom = Number(document.querySelector("select[name=denominator]").value);
-    this.coc = diagonal / denom;
+    var denom = Number(this.setting_val("denominator"));
+    this.CIRCLE_OF_CONFUSION = diagonal / denom;
 
     document.getElementById("diagonal").innerHTML = diagonal.toFixed(2) + "mm";
-    document.getElementById("acoc").innerHTML = this.coc.toFixed(3) + "mm";
+    document.getElementById("acoc").innerHTML = this.CIRCLE_OF_CONFUSION.toFixed(3) + "mm";
+
+    return this.CIRCLE_OF_CONFUSION;
 }
 
 
@@ -250,8 +294,8 @@ DepthOfFieldCalculator.prototype.set_next_stop = function(up, offset) {
 	return;
     }
     var i = Math.min(offset, stops.length-1);
-    cf.value = this.find_next_value(up, cf.value, stops[i]);
-    this.calculate();
+
+    this.change_setting(cf.name, this.find_next_value(up, cf.value, stops[i]));
 }
 
 DepthOfFieldCalculator.prototype.find_over = function(val, stops) {
@@ -362,33 +406,28 @@ DepthOfFieldCalculator.prototype.setup_keyboard_handling = function() {
 	}
     }
 
-    var fl = document.querySelector("input[name=focal_length]");
-    fl.onkeydown = function(e) {
+    self.setting_elt("focal_length").onkeydown = function(e) {
 	if (e.keyCode == 38) {
 	    //console.log("UP");
-	    fl.value = self.find_over(fl.value, self.FOCAL_LENGTHS);
-	    self.calculate();
+	    self.change_setting("focal_length", self.find_over(self.setting_val("focal_length"), self.FOCAL_LENGTHS));
 	} else if (e.keyCode == 40) {
 	    //console.log("DOWN");
-	    fl.value = self.find_under(fl.value, self.FOCAL_LENGTHS);
-	    self.calculate();
+	    self.change_setting("focal_length", self.find_under(self.setting_val("focal_length"), self.FOCAL_LENGTHS));
 	}
     }
 
-    var ap = document.querySelector("input[name=aperture]");
-    ap.onkeydown = function(e) {
+    self.setting_elt("aperture").onkeydown = function(e) {
 	if (e.keyCode == 38) {
 	    //console.log("UP");
 	    var stops = e.shiftKey ? self.HALF_STOP_APERTURES
 	                : e.altKey ? self.THIRD_STOP_APERTURES : self.APERTURES;
-	    ap.value = self.find_over(ap.value, stops);
-	    self.calculate();
+	    self.change_setting("aperture", self.find_over(self.setting_val("aperture"), stops));
+	    //self.calculate();
 	} else if (e.keyCode == 40) {
 	    //console.log("DOWN");
 	    var stops = e.shiftKey ? self.HALF_STOP_APERTURES
 	                : e.altKey ? self.THIRD_STOP_APERTURES : self.APERTURES;
-	    ap.value = self.find_under(ap.value, stops);
-	    self.calculate();
+	    self.change_setting("aperture", self.find_under(self.setting_val("aperture"), stops));
 	}
     }
 
@@ -411,19 +450,17 @@ DepthOfFieldCalculator.prototype.fmt = function(val) {
 DepthOfFieldCalculator.prototype.calculate = function() {
     //fdims = this.FORMATS[document.querySelector("select[name=format]").value];
     //var diagonal = Math.sqrt(Math.pow(fdims[0], 2) + Math.pow(fdims[1], 2));
-    //var coc = diagonal / 1730;
+    //var CIRCLE_OF_CONFUSION = diagonal / 1730;
 
-    var fl = Number(document.querySelector("input[name=focal_length]").value);
-    var ap = Number(document.querySelector("input[name=aperture]").value);
-    var hf = Math.pow(fl, 2) / (ap * this.coc) / 1000;
+    var fl = Number(this.setting_val("focal_length"));
+    var ap = Number(this.setting_val("aperture"));
+    var hf = Math.pow(fl, 2) / (ap * this.CIRCLE_OF_CONFUSION) / 1000;
 
-    var di = Number(document.querySelector("input[name=distance]").value);
+    var di = Number(this.setting_val("distance"));
     if (di <= 0) {
 	di = hf;
-	//document.querySelector("input[name=distance]").value = di.toFixed(2);
     } else if (di < fl * 0.002) {
 	di = fl * 0.002;
-	//document.querySelector("input[name=distance]").value = di.toFixed(2);
     }
 
     var dofn, doff;
